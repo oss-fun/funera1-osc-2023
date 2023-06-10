fea8df2c70bdc269796a9eac9866f08a6123d00e
funera1 <60760935+funera1@users.noreply.github.com>
2023-06-10 14:24:30 +0900

    Delete cmake_build

--- cmake_build/CMakeFiles/basic.dir/wasm-micro-runtime/core/shared/utils/uncommon/bh_getopt.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/basic.dir/wasm-micro-runtime/core/shared/utils/uncommon/bh_getopt.c.o: \
- /home/funera1/repo/funera1-osc-2023/wasm-micro-runtime/core/shared/utils/uncommon/bh_getopt.c \
- /usr/include/stdc-predef.h

d73cc193c737b0628ac35d23424c7f5bed81158e
funera1 <funeral7776@gmail.com>
2023-06-13 09:36:28 +0900

    Feat: Asynchronous execution of wasm application

--- controller/run.py
+++ controller/run.py
@@ -3,7 +3,6 @@ import signal
 import time
 import asyncio
 
-
 async def run():
     wasm_executor = "../cmake_build/basic"
     wasm_app = "../wasi-lifegame/target/wasm32-wasi/debug/wasi-lifegame.wasm"
@@ -14,22 +13,9 @@ async def run():
             *args,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
-
         )
 
-    # while True:
-    #     stdout = (await proc.stdout.readline()).decode()
-    #     if stdout:
-    #         print(f'[stdout] {stdout}', end='', flush=True)
-    #     stderr = (await proc.stderr.readline()).decode()
-    #     if stderr:
-    #         print(f'[sdterr] {stderr}', end='', flush=True, file=sys.stderr)
-    # 
-    #     await asyncio.sleep(1)
     return proc
-
-    # proc = subprocess.run([wasm_executor, "-f", wasm_app])
-    # return proc
 
 
 def signal_handler(signum, frame):
@@ -39,8 +25,11 @@ def stop(pid):
     signal.signal(signal.SIGINT, signal_handler)
 
 proc = asyncio.run(run())
+print("return async run")
 
-time.sleep(10)
+time.sleep(1)
+print("sleep(1)")
 
-stop(proc)
-
+# stop(proc)
+proc.send_signal(signal.SIGINT)
+print("stop process")
